a287ecee8fc87cad3ade9998cd96fc195cb36ba4
Bailin Huang <bailinhuangq@hotmail.com>
2019-07-14 14:48:35 -0600

    added sidenav and fixed routing

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {MatDialog} from '@angular/material';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  username: string;
+  password: string;
+  ngOnInit() {
+  }
+
+  login(): void {
+    if(this.username === 'admin' && this.password === 'admin'){
+     this.router.navigate(['core']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+}
