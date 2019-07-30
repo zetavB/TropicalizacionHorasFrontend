4dc66602aa92633b5d5a608f9b306102f676bbd7
Bailin Huang <bailinhuangq@hotmail.com>
2019-07-30 11:11:22 -0600

    se muestra los proyectos bien, pero hay un error

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Estudiante } from '../models/estudiante.model';
-import { Response } from '../models/response.model'; 
+import { Response } from '../models/response.model';
 import {catchError, map} from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +15,26 @@ export class UserService {
 
   }
 
-  private url = 'http://localhost:8080/estudiante/';
-  // private response: Response;
+  private url =  environment.serverUrl + '/estudiante/';
+
+  projectToStringArray(projects) {
+    let array = [];
+    projects.forEach(
+      project => array.push(project.nombre.toString())
+    );
+    return array;
+  }
 
   getResponse(id: string): Observable<Estudiante> {
     return this.http.get<Response>(this.url + id).pipe(
       map(
         response => {
+          const projectsArray = this.projectToStringArray(response.response.proyectos);
           const estudiante: Estudiante = {
             tipo: response.response.tipo,
             estado: response.response.estado,
             horasTotales: response.response.horasTotales,
-            proyectos: response.response.proyectos,
+            proyectos: projectsArray,
             fechaFinal: response.response.fechaFinal,
             fechaInicio: response.response.fechaInicio,
             carne: response.response.carne,
